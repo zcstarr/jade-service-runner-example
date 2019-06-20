3307baadfc5f28998cae33447a25da2ff8025571
Zane Starr <zcstarr@gmail.com>
2019-06-20 14:09:15 -0700

    feat: add shell example

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
-export function helloWorld() {
-  // hello world
-  return "hello world";
-}
+const unit = require("ethjs-unit");
 
-export default helloWorld;
+const address = process.argv[2];
+const showBalance = (balance: string) => {
+  // tslint:disable-next-line:no-console
+  console.log(unit.fromWei(balance, "ether").toString(10));
+};
+
+const start = async () => { };
+
+start();
