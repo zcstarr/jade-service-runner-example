72594ecbdc4949cd7bbc5d96ddc25a4a3c38eec1
ia <isaac.ardis@gmail.com>
2019-10-25 17:12:57 -0500

    save

--- src/index.ts
+++ src/index.ts
@@ -18,20 +18,20 @@ const erpc = new EthereumJSONRPC({
     host: "localhost",
     port: 8002,
     type: "http",
-    path: "multi-geth/mainnet/1.9.2",
+    path: "multi-geth/kotti/1.9.2",
   },
 });
 
 console.log("starting client");
-client.installService("multi-geth", "1.9.0")
-  .then(() => client.listInstalledServices())
+client.installService("multi-geth", "1.9.2")
+  // .then(() => client.listInstalledServices())
+  // .then(() => client.listRunningServices())
+  // .then(console.log) //tslint:disable-line
+  .then(() => client.startService("multi-geth", "1.9.2", "kotti"))
+  // .then(console.log)//tslint:disable-line
   .then(() => client.listRunningServices())
-  .then(console.log) //tslint:disable-line
-  .then(() => client.startService("multi-geth", "1.9.0", "mainnet"))
-  .then(console.log)//tslint:disable-line
-  .then(() => client.listRunningServices())
-  .then(console.log)//tslint:disable-line
- client.startService("multi-geth", "1.9.0", "mainnet")
+  // .then(console.log)//tslint:disable-line
+ client.startService("multi-geth", "1.9.2", "kotti")
   .then(() => erpc.eth_getBalance("0xc1912fee45d61c87cc5ea59dae31190fffff232d", "0x0"))
   .then(console.log)
   .catch((e) => {
